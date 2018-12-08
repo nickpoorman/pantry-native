aad165c315422c6d85970a75a4b5b7e20987ecac
Nick Poorman <nickpoorman@gmail.com>
2018-12-08 11:45:35 -0500

    Got cards layed out properly

--- Pantry/screens/HomeScreen.js
+++ Pantry/screens/HomeScreen.js
@@ -29,15 +29,38 @@ export default class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
         <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}
+          style={styles.scrollContainer}
+          contentContainerStyle={styles.scrollContentContainer}
         >
-          <View>
-            <Text>This is a tab bar. You can edit it in:</Text>
+          <View style={styles.card}>
+            <Image
+              source={require("../assets/images/piechart.png")}
+              style={styles.iconImage}
+            />
+            <View style={styles.cardText}>
+              <Text style={styles.cardTextPrimary}>40 Orders</Text>
+              <Text style={styles.cardTextSecondary}>38 shipped</Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardText}>
+              <Text style={styles.cardTextPrimary}>40 Orders</Text>
+              <Text style={styles.cardTextSecondary}>38 shipped</Text>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <View style={styles.cardText}>
+              <Text style={styles.cardTextPrimary}>40 Orders</Text>
+              <Text style={styles.cardTextSecondary}>38 shipped</Text>
+            </View>
+            <Image
+              source={require("../assets/images/piechart.png")}
+              style={styles.iconImage}
+            />
           </View>
         </ScrollView>
 
-        <View style={styles.tabBarInfoContainer}>
+        {/* <View style={styles.tabBarInfoContainer}>
           <Text style={styles.tabBarInfoText}>
             This is a tab bar. You can edit it in:
           </Text>
@@ -49,7 +72,7 @@ export default class HomeScreen extends React.Component {
               navigation/MainTabNavigator.js
             </MonoText>
           </View>
-        </View>
+        </View> */}
       </View>
     );
   }
@@ -91,6 +114,54 @@ export default class HomeScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  scrollContainer: {
+    flex: 1,
+    backgroundColor: "#f0f0f0"
+  },
+  scrollContentContainer: {
+    paddingTop: 30
+  },
+  chartBox: {
+    backgroundColor: "#f0f0f0"
+  },
+  card: {
+    marginTop: 17,
+    marginLeft: 17,
+    marginRight: 17,
+    padding: 8,
+    backgroundColor: "#ffffff",
+    flexDirection: "row"
+  },
+  iconImage: {
+    width: 40,
+    height: 40,
+    resizeMode: "contain",
+    margin: 8
+  },
+  cardText: {
+    flexDirection: "column",
+    padding: 8,
+    justifyContent: "center"
+  },
+  cardTextPrimary: {
+    fontSize: 17,
+    color: "#222325",
+    lineHeight: 24
+    // textAlign: "center"
+  },
+  cardTextSecondary: {
+    fontSize: 12,
+    color: "#a3a3a3"
+    // lineHeight: 18
+    // textAlign: "center"
+  }
+});
+
+const oldStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
