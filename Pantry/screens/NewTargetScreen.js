62af7c8f78340e2d171a446c0a8aad604f4ea0d8
Nick Poorman <nickpoorman@gmail.com>
2019-01-13 11:28:03 -0500

    Added form for creating targets

--- Pantry/screens/NewTargetScreen.js
+++ Pantry/screens/NewTargetScreen.js
@@ -1,25 +1,104 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text, FlatList } from 'react-native'
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  FlatList,
+  TextInput,
+  Button,
+  View,
+  Platform,
+} from 'react-native'
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { purple, lightestPurple } from 'app/styles'
+import Layout from 'app/constants/Layout'
 
 export default class NewTargetScreen extends React.Component {
   static navigationOptions = {
     title: 'New Target',
   }
 
+  state = {
+    url: '',
+  }
+
   render() {
     const data = Array.from({ length: 200 }, (_, n) => ({ key: `row-${n}` }))
     return (
       <ScrollView style={styles.container}>
-        <Text>TODO. New target screen</Text>
+        <View style={styles.formContainer}>
+          <MaterialCommunityIcons
+            name='json'
+            size={90}
+            color={purple}
+            style={styles.icon}
+          />
+
+          <Text style={styles.formDescription}>
+            Creating a new target will add another endpoint to fetch metrics
+            from.
+          </Text>
+
+          <View style={styles.fieldsContainer}>
+            <TextInput
+              style={styles.formField}
+              placeholder='Enter a name for the target'
+              onChangeText={url => this.setState({ url })}
+            />
+          </View>
+
+          <View style={styles.fieldsContainer}>
+            <TextInput
+              style={styles.formField}
+              placeholder='Enter the URL for the target'
+              onChangeText={url => this.setState({ url })}
+              autoCapitalize='none'
+              autoCorrect={false}
+              keyboardType={Platform.OS === 'ios' ? 'url' : 'default'}
+              returnKeyType='done'
+            />
+          </View>
+
+          <Button
+            onPress={() => {
+              // Dispatch a create target from the text
+            }}
+            title='Create Target!'
+            color={purple}
+          />
+        </View>
       </ScrollView>
     )
   }
 }
 
+// We don't want the form to be covered by the keyboard.
+const maxHeight = Layout.window.height / 2 - (Layout.window.height / 2) * 0.1
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  formContainer: {
+    height: maxHeight,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  formDescription: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  fieldsContainer: {
+    flexDirection: 'row',
+  },
+  formField: {
+    flexGrow: 1,
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: lightestPurple,
   },
 })
